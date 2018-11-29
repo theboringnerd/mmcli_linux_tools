d36be477e6a6510dd66d55d46943084d5d54d347
sherlock <wisdomnji@gmail.com>
2018-11-29 21:04:51 +0100

    changes

--- SMS/main.cxx
+++ SMS/main.cxx
@@ -8,23 +8,74 @@ using namespace std;
 
 
 int main(int argc, char** argv) {
-	//change from modem file without removing the entire libraries
+
+	bool send = false;
+	string msg, number, cls = "1";
+
+	if(argc < 2) {
+		cout << "Pass in commands\nExiting!" << endl;
+		return 1;
+	}
+
+	for(int i=0;i<argc;i++) {
+		if((string)argv[i] == "--send") {
+			send = true;
+		}
+		else if((string)argv[i] == "--message") {
+			msg = (string)argv[i+1];
+			i++;
+			continue;
+		}
+		else if((string)argv[i] == "--number") {
+			number = (string)argv[i+1];
+			i++;
+			continue;
+
+		}
+		else if((string)argv[i] == "--class") {
+			cls = (string)argv[i+1];
+			i++;
+			continue;
+		
+		}
+	}
+
+	if(send) {
+		if(!msg.empty())
+			if(!number.empty())
+				cout << "Commands received all good!" << endl;
+			else {
+				cout << "Number not set!" << endl;
+				return 1;
+			}
+		else {
+			cout << "Message not set!" << endl;
+			return 1;
+		}
+	}
+	else {
+		cout << "Not a valid input operation!" << endl;
+		return 1;
+	}
+	
+	
 	Modem modem = Modem::init("E3131A");
 
 	if(modem.good()) cout << "Modem found and ready!" << endl;
 	else cout << "Modem not found or not ready!" << endl;
 
 	SMS sms = SMS::init(modem);
-	
-	sms.set_msg("hello world");
-	sms.set_number("652156811");
-	sms.set_class("0");
+
+	sms.set_msg(msg);
+	sms.set_number(number);
+	sms.set_class(cls);
 	sms.prepare();
 	if(sms.good()) sms.send();
 	else {
 		cout << "sms is not configured correctly!! ENDING" << endl;
 		return 1;
 	}
+	
 
 	return 0;
 }
